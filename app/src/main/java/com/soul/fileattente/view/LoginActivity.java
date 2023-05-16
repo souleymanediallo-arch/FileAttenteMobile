7aa41436fbdd7e9db643e9fba4dc55136f03fa5f
X2028560 <souleymane.diallo@artp.fr>
2023-05-16 18:52:07 +0200

    FileAttente -> Working version Cleaned #2 - Voir commentaire dans la classe UserViewModel.java

--- app/src/main/java/com/soul/fileattente/view/LoginActivity.java
+++ app/src/main/java/com/soul/fileattente/view/LoginActivity.java
@@ -20,44 +20,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
     private UserViewModel userViewModel;
-    //private Observer<User> userObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_login);
         super.onCreate(savedInstanceState);
+
+        //Using ViewBinding to manage Layout Components
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        //binding.setLifecycleOwner(this);
 
         //Precess Btnlogin Click
         processTaskWhenloginButtonClicked();
 
-        //Getting Instance of the viewModel
+        //Getting Instance of the viewModel that will manage the Business of the aapplication
         userViewModel = new ViewModelProvider(LoginActivity.this).get(UserViewModel.class);
-        userViewModel.init();
-        //userViewModel.doAgain();
 
-        final Observer<User> userObserver = new Observer<User>() {
-            @Override
-            public void onChanged(User user) {
-                //Update UI Here
-                binding.edtxtUsername.setText(user.getUserName());
-                binding.edtxtPassword.setText(user.getUserPwd());
-                Toast.makeText(LoginActivity.this, "Data has Changed --> " + user.toString(), Toast.LENGTH_LONG).show();
-                //just log this
-                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Name = " + user.getUserName());
-                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Pwd = " + user.getUserPwd());
-                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Status = " + user.isUserStatus());
-            }
-        };
-        userViewModel.getmUser().observe(LoginActivity.this, userObserver);
         //Be prepared to process the changes of the values for var in viewModel
-        //processWhenDataChanged();
+        processWhenDataChanged();
 
-
+        //Process with initialisation
+        userViewModel.init();
     }
 
     void processTaskWhenloginButtonClicked(){
@@ -65,45 +48,23 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 userViewModel.doAgain();
-//                //System.out.printf("------------------------------------> ");
-//                Toast.makeText(LoginActivity.this, "Just Clicked", Toast.LENGTH_SHORT).show();
-//                //binding.btnLogin.setText("Just clicked ! ");
-//                User sampleUser = new User();
-//                sampleUser.setUserName("ZAHRA");
-//                sampleUser.setUserPwd("Aminata");
-//                sampleUser.setUserStatus(true);
-//                userViewModel.getmUser().setValue(sampleUser);
             }
         });
     }
 
     void processWhenDataChanged(){
-
-//        userObserver = new Observer<User>() {
-//            @Override
-//            public void onChanged(User user) {
-//                //Update UI Here
-//                binding.edtxtUsername.setText(user.getUserName());
-//                binding.edtxtPassword.setText(user.getUserPwd());
-//                Toast.makeText(LoginActivity.this, "Data has Changed", Toast.LENGTH_SHORT).show();
-//                //just log this
-//                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Name = " + user.getUserName());
-//                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Pwd = " + user.getUserPwd());
-//                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Status = " + user.isUserStatus());
-//            }
-//        };
-//        userViewModel.getmUser().observe(this, new Observer<User>() {
-//            @Override
-//            public void onChanged(User user) {
-//                //Update UI Here
-//                binding.edtxtUsername.setText(user.getUserName());
-//                binding.edtxtPassword.setText(user.getUserPwd());
-//                Toast.makeText(LoginActivity.this, "Data has Changed", Toast.LENGTH_SHORT).show();
-//                //just log this
-//                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Name = " + user.getUserName());
-//                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Pwd = " + user.getUserPwd());
-//                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Status = " + user.isUserStatus());
-//            }
-//        });
+        userViewModel.getmUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                //Update UI Here
+                binding.edtxtUsername.setText(user.getUserName());
+                binding.edtxtPassword.setText(user.getUserPwd());
+                Toast.makeText(LoginActivity.this, "Data has Changed", Toast.LENGTH_SHORT).show();
+                //just log this
+                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Name = " + user.getUserName());
+                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Pwd = " + user.getUserPwd());
+                Logger.getLogger(getLocalClassName()).log(Level.INFO,"------------------------------------> I got changed and my current value is Status = " + user.isUserStatus());
+            }
+        });
     }
 }
--- app/src/main/java/com/soul/fileattente/viewmodel/UserViewModel.java
+++ app/src/main/java/com/soul/fileattente/viewmodel/UserViewModel.java
@@ -9,34 +9,27 @@ import com.soul.fileattente.model.User;
 import com.soul.fileattente.repository.UserRepository;
 
 //Write all Business Logic here and free the view from that
+//Veiller à utiliser les MutableLiveData et liveData uniquement dans la ViewModel et surtout pas
+//Permettre à d'autres classe de renvoyer des MutableLive Data or des Live Data
+//D'aielleurs cela permet de ne pas denaturer ces Classes qui au demeurant ne savent rien des LiveData
+//Et devraient pouvoir etre utilisées à d'autres endroits
 public class UserViewModel extends ViewModel {
 
-    private MutableLiveData<User> mUser;
+    private User aGivenUser;
+    private MutableLiveData<User> mUser = new MutableLiveData<>();
     private UserRepository mUserRepo;
 
-
     public void init(){
-        if(mUser != null){
-            return;
-        }else {
-            mUserRepo = UserRepository.getInstance();
-            mUser = mUserRepo.getSelectedUserAsMutableLiveData(0);
-        }
+        mUserRepo = UserRepository.getInstance();
+        aGivenUser = mUserRepo.getSelectedUser(0);
+        mUser.postValue(aGivenUser);
     }
 
     @MainThread
     public void doAgain(){
-//        if(mUser != null){
-//            System.out.println("------------------------------------> 01");
-//            return;
-//        }else {
-            System.out.println("------------------------------------> 01");
             mUserRepo = UserRepository.getInstance();
-            //mUser = mUserRepo.getSelectedUserAsMutableLiveData(1);
             User sampleUser = mUserRepo.getSelectedUser(1);
-            //mUser.setValue(sampleUser);
             mUser.postValue(sampleUser);
-//        }
     }
 
     public LiveData<User> getmUser() {
